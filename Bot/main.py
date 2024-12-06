3ab5d16516062689e4565bc709dd0e48fa490c15
Liam <mountnRocky@gmail.com>
2024-12-06 13:36:06 -0700

    Added basi teamsearch command

--- Bot/main.py
+++ Bot/main.py
@@ -144,27 +144,72 @@ async def season_info(ctx: discord.Interaction, *, team_number: int, season: int
         await ctx.followup.send(embed=embed)
         return
     
-    total = data['data']['teamByNumber']['quickStats']['tot']
-    autonomous = data['data']['teamByNumber']['quickStats']['auto']
-    driver_control = data['data']['teamByNumber']['quickStats']['dc']
-    end_game = data['data']['teamByNumber']['quickStats']['eg']
-    count = data['data']['teamByNumber']['quickStats']['count']
+    quickStats = data['data']['teamByNumber']['quickStats']
+    if quickStats:
+        total = quickStats['tot']
+        autonomous = quickStats['auto']
+        driver_control = quickStats['dc']
+        end_game = quickStats['eg']
+        count = quickStats['count']
 
-    embed = discord.Embed(title=f"Season Stats for {team_number}", description=f":calendar_spiral:  Season: {season}", )
-    if total:
-        embed.add_field(name="Total Score:", value=f":1234: OPR: {round(total['value'], 3)} \n ---------- \n :medal: Rank: {total['rank']}", inline=False)
-    if autonomous:
-        embed.add_field(name="Auto:", value=f":1234: OPR: {round(autonomous['value'], 3)} \n ---------- \n :medal: Rank: {autonomous['rank']}", inline=True)
-    if driver_control:
-        embed.add_field(name="TeleOp:", value=f":1234: OPR: {round(driver_control['value'], 3)} \n ---------- \n :medal: Rank: {driver_control['rank']}", inline=True)
-    if end_game:
-        embed.add_field(name="End Game:", value=f":1234: OPR: {round(end_game['value'], 3)} \n ---------- \n :medal: Rank: {end_game['rank']}", inline=True)
+        embed = discord.Embed(title=f"Season Stats for {team_number}", description=f":calendar_spiral:  Season: {season}", )
+        if total:
+            embed.add_field(name="Total Score:", value=f":1234: OPR: {round(total['value'], 3)} \n ---------- \n :medal: Rank: {total['rank']}", inline=False)
+        if autonomous:
+            embed.add_field(name="Auto:", value=f":1234: OPR: {round(autonomous['value'], 3)} \n ---------- \n :medal: Rank: {autonomous['rank']}", inline=True)
+        if driver_control:
+            embed.add_field(name="TeleOp:", value=f":1234: OPR: {round(driver_control['value'], 3)} \n ---------- \n :medal: Rank: {driver_control['rank']}", inline=True)
+        if end_game:
+            embed.add_field(name="End Game:", value=f":1234: OPR: {round(end_game['value'], 3)} \n ---------- \n :medal: Rank: {end_game['rank']}", inline=True)
+
+        if count:
+            embed.add_field(name=f"Count:", value=f"Rank is out {count} teams", inline=False)
+    else:
+        embed = discord.Embed(title=f"Season Stats for {team_number}", description=f"No quick stats available for season {season}", color=0xfc585b)
 
     embed.set_footer(text=universal_footer)
 
-    if count:
-        embed.add_field(name=f"Count:", value=f"Rank is out {count} teams", inline=False)
+    await ctx.followup.send(embed=embed)
 
+@bot.tree.command(name="teamsearch", description="Search for a team by name")
+async def team_search(ctx: discord.Interaction, *, team_name: str, limit: int = 3, season: int = 2024):
+    await ctx.response.defer()
+    query = '''
+    query teamSearch {
+        teamsSearch(searchText: "''' + team_name + '''", limit: ''' + str(limit) +''') {
+            number
+            name
+            quickStats(season: ''' + str(season) + ''') {
+                tot{value, rank}
+            }
+        }  
+    }
+    '''
+
+    response = requests.post(URL, json={'query': query})
+    data = response.json()
+
+    teams = data['data']['teamsSearch']
+
+    embed = discord.Embed(title=f"Teams with the name: {team_name}", description=f"Season: {season}", color=0x00ff00)
+
+    if teams:
+        teams_description = ""
+        for team in teams:
+            team_info = f"**Team #:** {team['number']} \n **Team Name:** {team['name']} \n"
+            quickStats = team.get('quickStats')
+            if quickStats:
+                total = quickStats.get('tot')
+                if total:
+                    team_info += f"**Total OPR:** {round(total['value'], 3)} \n **Overall Rank:** {total['rank']} \n"
+            team_info += "\n"
+            if len(teams_description) + len(team_info) > 1024:
+                break
+            teams_description += team_info
+        if teams_description:
+            embed.add_field(name="Teams", value=teams_description, inline=False)
+    
+    embed.set_footer(text=universal_footer)
     await ctx.followup.send(embed=embed)
 
 @bot.tree.command(name="gamemanual", description="Get a link to the game manual")
