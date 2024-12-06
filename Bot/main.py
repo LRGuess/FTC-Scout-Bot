256bd7c924dd75257e322e0337e784e2da749687
Liam <mountnRocky@gmail.com>
2024-12-06 14:55:07 -0700

    Fixed the too many awards bug

--- Bot/main.py
+++ Bot/main.py
@@ -124,6 +124,7 @@ async def team_info_by_number(ctx: discord.Interaction, *, team_number: int):
     # Awards
     awards = data['data']['teamByNumber']['awards']
 
+    embeds = []
     embed = discord.Embed(title="Team: " + str(team_number), description="")
 
     if team_name:
@@ -137,19 +138,36 @@ async def team_info_by_number(ctx: discord.Interaction, *, team_number: int):
     if website:
         embed.add_field(name=":globe_with_meridians: Website", value=website, inline=True)
     if city and state and country:
-        embed.add_field(name=":round_pushpin: Location", value=f"{city}, {state}, {country} \n -----------------------------------------------------", inline=True)
+        embed.add_field(name=":round_pushpin: Location", value=f"{city}, {state}, {country}", inline=True)
+
+    embeds.append(embed)
 
     # Add awards to the embed
     if awards:
         awards_description = ""
         for award in awards:
-            awards_description += f"**:calendar_spiral: Season:** {award['season']} \n **:round_pushpin: Event:** {award['event']['name']} \n **:receipt: Type:** {award['type']} \n **:military_medal: Placement:** {award['placement']}\n \n"
+            award_info = f"**:calendar_spiral: Season:** {award['season']} \n **:round_pushpin: Event:** {award['event']['name']} \n **:receipt: Type:** {award['type']} \n **:military_medal: Placement:** {award['placement']}\n \n"
+            if len(awards_description) + len(award_info) > 1024:
+                embed = discord.Embed(title="Team: " + str(team_number), description="")
+                embed.add_field(name="Awards", value=awards_description, inline=False)
+                embed.set_footer(text=universal_footer)
+                embeds.append(embed)
+                awards_description = award_info
+            else:
+                awards_description += award_info
         if awards_description:
-            embed.add_field(name="Awards", value=f"{awards_description} -----------------------------------------------------", inline=False)
+            embed = discord.Embed(title="Team: " + str(team_number), description="")
+            embed.add_field(name="Awards", value=awards_description, inline=False)
+            embed.set_footer(text=universal_footer)
+            embeds.append(embed)
 
-    embed.set_footer(text=universal_footer)
-
-    await ctx.followup.send(embed=embed)
+    if embeds:
+        view = Paginator(embeds)
+        await ctx.followup.send(embed=embeds[0], view=view)
+    else:
+        embed = discord.Embed(title="Team: " + str(team_number), description="No awards found.", color=0xff0000)
+        embed.set_footer(text=universal_footer)
+        await ctx.followup.send(embed=embed)
 
 @bot.tree.command(name='seasoninfo', description='Get season statistics for a certain team')
 async def season_info(ctx: discord.Interaction, *, team_number: int, season: int = 2024):
