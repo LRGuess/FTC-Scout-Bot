6c94e4954bf6d91a0378be0513ee3560051703c4
Liam <mountnRocky@gmail.com>
2024-12-07 11:47:09 -0700

    Updated Help

--- Bot/main.py
+++ Bot/main.py
@@ -525,13 +525,15 @@ async def about(ctx: discord.Interaction):
 async def help(ctx: discord.Interaction):
     await ctx.response.defer()
 
-    embed = discord.Embed(title="Help", description="Here are the commands you can use with the bot", color=0x00ff00)
-    embed.add_field(name="/teaminfo <team_number>", value="Get general information about a team", inline=False)
-    embed.add_field(name="/seasoninfo <team_number> <season>", value="Get season statistics for a team", inline=False)
-    embed.add_field(name="/teamsearch <team_name>", value="Search for a team by name", inline=False)
-    embed.add_field(name="/eventsearch <event_name>", value="Search for an event by name", inline=False)
+    embed = discord.Embed(title="Help", description="Here are the commands you can use with the bot. \n Anything with an * is required \n Any anspecified season will default to the current one", color=0x00ff00)
+    embed.add_field(name="/teaminfo <team_number>*", value="Get general information about a team", inline=False)
+    embed.add_field(name="/seasoninfo <team_number>* <season>", value="Get season statistics for a team", inline=False)
+    embed.add_field(name="/teamsearch <team_name>* <limit> <season>", value="Search for a team by name. Limit limits the amount of results shown", inline=False)
+    embed.add_field(name="/eventsearch <event_name>* <limit> <season>", value="Search for an event by name. Limit limits the amount of results shown", inline=False)
+    embed.add_field(name="/eventinfo <event_code>* <season> <show_teams> <show_matches> <show_awards>", value="Get information about an event. If show_matches is True, the embed will display info for all matches. If show_teams is True, embed will show all teams. If show_awards is True, embed will show the awards granted.", inline=False)
     embed.add_field(name="/gamemanual", value="Get a link to the game manual", inline=False)
     embed.add_field(name="/about", value="Get information about the bot", inline=False)
+    embed.add_field(name="/help", value="Get help with the bot", inline=False)
 
     embed.set_footer(text=universal_footer)
 
