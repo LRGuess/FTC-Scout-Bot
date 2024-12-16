e8e796b70a5d54c4670aa17f889f9a950de3a8f5
Liam <mountnRocky@gmail.com>
2024-12-16 07:37:39 -0700

    Updated Help Command

--- Bot/Commands/help.py
+++ Bot/Commands/help.py
@@ -10,13 +10,17 @@ async def help(ctx: discord.Interaction):
 
     embed = discord.Embed(title="Help", description="Here are the commands you can use with the bot. \n Anything with an * is required \n Any anspecified season will default to the current one", color=0x00ff00)
     embed.add_field(name="/teaminfo <team_number>*", value="Get general information about a team", inline=False)
+    embed.add_field(name="/teaminfobyname <team_name>*", value="Get general information about a team", inline=False)
     embed.add_field(name="/seasoninfo <team_number>* <season>", value="Get season statistics for a team", inline=False)
     embed.add_field(name="/teamsearch <team_name>* <limit> <season>", value="Search for a team by name. Limit limits the amount of results shown", inline=False)
     embed.add_field(name="/eventsearch <event_name>* <limit> <season>", value="Search for an event by name. Limit limits the amount of results shown", inline=False)
+    embed.add_field(name="/worldrecord <season>", value="Get the world record for a certain season", inline=False)
+    embed.add_field(name="/matchesplayed <season>", value="Get the amount of matches played in a season", inline=False)
+    embed.add_field(name="/inspect", value="Runs Robot Inspection", inline=False)
     embed.add_field(name="/eventinfo <event_code>* <season> <show_teams> <show_matches> <show_awards>", value="Get information about an event. If show_matches is True, the embed will display info for all matches. If show_teams is True, embed will show all teams. If show_awards is True, embed will show the awards granted.", inline=False)
     embed.add_field(name="/gamemanual", value="Get a link to the game manual", inline=False)
     embed.add_field(name="/about", value="Get information about the bot", inline=False)
-    embed.add_field(name="/help", value="Get help with the bot", inline=False)
+    embed.add_field(name="/help", value="Shows this dialogue", inline=False)
 
     embed.set_footer(text=universal_footer)
 
